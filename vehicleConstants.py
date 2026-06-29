e2101ec7d7bb5a2e23c0f7543db36af36bd9f198
Juan Pablo Pedroni <jppedroni@siana-systems.com>
2026-06-28 22:42:19 -0300

    Update vehicle speed constant and enhance dashboard layout; adjust speed input value and button arrangement

--- vehicleConstants.py
+++ vehicleConstants.py
@@ -1,7 +1,7 @@
 
 L_car = 24.0 #[cm] Distancia ente ejes
 psi_max = 20.0 #[°] Máximo radio de giro
-v = 39.0 #[cm/seg] Velocidad lineal constante del vehículo
+v = 40.0 #[cm/seg] Velocidad lineal constante del vehículo
 #r_turn_min = 70.1713    # L_car/sin(psi_max), radio mínimo de giro
 r_turn_min = 80.0        # Hacemos el radio de giro mínimo un poco más grande para darle espacio al control de dirección
 
